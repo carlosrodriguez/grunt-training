35c080553c6ebb851896d1902db0ea90086cf736
Carlos Rodriguez <me@carlosrodriguez.us>
2013-06-27 12:55:57 -0400

    Added basic variables to the gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,16 @@ module.exports = function (grunt) {
 
 	// Project configuration.
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
+		files: {
+			source: './source',
+			css: '<%= files.source %>/css',
+			js: [
+				"<%= files.source %>/simple/**/*.js",
+				"<%= files.source %>/advanced/**/*.js"
+			]
+		}
 	});
 	
 	grunt.registerTask('default', []);
