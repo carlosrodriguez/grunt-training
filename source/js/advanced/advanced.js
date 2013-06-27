a6fb700be93d8a75537a4a15881af93c790e9ec3
Carlos Rodriguez <me@carlosrodriguez.us>
2013-06-27 13:18:24 -0400

    Added some sample source files

---
+++ source/js/advanced/advanced.js
@@ -0,0 +1,22 @@
+/*
+* File name:   advanced.js
+* Description: Some crazy advanced js
+* Version:     1.0
+*/
+
+advanced = (function() {
+
+	function advanced() {}
+
+	advanced.prototype.init = function() {
+		this.showHowAdvancedIAm();
+	};
+
+	advanced.prototype.showHowAdvancedIAm = function() {
+
+	};
+})();
+
+wow = new advanced();
+
+wow.init();
---
+++ source/js/simple/simple.js
@@ -0,0 +1,9 @@
+/*!
+* File name:   simple.js
+* Description: Some simple js
+* Version:     1.0
+*/
+
+$('document').ready(function(){
+	console.log("Wow I am simple");
+});
